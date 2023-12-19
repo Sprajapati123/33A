3462e3728c231e81acec6aa815461f1fc95cb169
sprajapati123 <sandisprajapati52@gmail.com>
2023-12-19 09:11:16 +0545

    posting data in firebase

--- lib/form/form_screen.dart
+++ lib/form/form_screen.dart
@@ -13,36 +13,50 @@ class _FormScreenState extends State<FormScreen> {
   TextEditingController fnameController = TextEditingController();
   TextEditingController lnameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
-
+  final formKey = GlobalKey<FormState>();
   final database = FirebaseDatabase.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Registration form"),),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("first name"),
-          TextFormField(controller: fnameController),
-          Text("last name"),
-          TextFormField(controller: lnameController),
-          Text("email"),
-          TextFormField(controller: emailController),
+      body: Form(
+        key: formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("first name"),
+            TextFormField(
+              validator: (value){
+                if(value == ""){
+                  return "enter text";
+                }
+                return null;
+              },
+                controller: fnameController),
+            Text("last name"),
+            TextFormField(controller: lnameController),
+            Text("email"),
+            TextFormField(controller: emailController),
 
-          ElevatedButton(onPressed: () async{
-            var data = {
-              "firstname": fnameController.text,
-              "lastname": lnameController.text,
-              "email": emailController.text,
-            };
-            await database.ref().child("contact").push().set(data).then((value) {
-              print("Success");
-            }).onError((error, stackTrace) {
-              print(error.toString());
-            });
-          }, child: Text("Submit"))
-        ],
+            ElevatedButton(onPressed: () async{
+              if(formKey.currentState!.validate()){
+                var data = {
+                  "firstname": fnameController.text,
+                  "lastname": lnameController.text,
+                  "email": emailController.text,
+
+                };
+                await database.ref().child("contact").push().set(data).then((value) {
+                  print("Success");
+                }).onError((error, stackTrace) {
+                  print(error.toString());
+                });
+              }
+
+            }, child: Text("Submit"))
+          ],
+        ),
       ),
     );
   }
