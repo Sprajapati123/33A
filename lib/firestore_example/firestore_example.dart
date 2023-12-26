044311ba7a2ebaaaf86129c3169d2d718d4e9f51
sprajapati123 <sandisprajapati52@gmail.com>
2023-12-26 10:49:56 +0545

    delete

--- lib/firestore_example/firestore_example.dart
+++ lib/firestore_example/firestore_example.dart
@@ -21,30 +21,61 @@ class _FireStoreExampleState extends State<FireStoreExample> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("first name"),
-          TextFormField(
-              controller: fnameController),
-          Text("last name"),
-          TextFormField(controller: lnameController),
-          Text("email"),
-          TextFormField(controller: emailController),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ///List<String> data = ["a","b","c"]
+            /// for (i =0 ;i<data.length;i++){
+            /// print(data[i]);
+            /// }
+            ///
+            /// for(var a in data){
+            /// print(a)
+            /// }
+            ///
+            StreamBuilder(
+              stream: firestore.collection('users').snapshots(),
+              builder: (context, snapshot) {
+                return ListView(
+                  physics: const ScrollPhysics(),
+                  shrinkWrap: true,
+                  children: snapshot.data!.docs
+                      .map((DocumentSnapshot e) => ListTile(
+                            title: Text(e['email']),
+                            subtitle: Text(e['firstname']),
+                    trailing: IconButton(icon: Icon(Icons.delete),onPressed: (){
+                      firestore.collection('users').doc(e.id).delete();
+                    }),
+                          ))
+                      .toList(),
+                );
+              },
+            ),
+            Text("first name"),
+            TextFormField(controller: fnameController),
+            Text("last name"),
+            TextFormField(controller: lnameController),
+            Text("email"),
 
-          ElevatedButton(onPressed: () async{
 
-            // FirebaseDatabase.instance.ref().child("").set(value);
-            var data = {
-              "firstname": fnameController.text,
-              "lastname": lnameController.text,
-              "email": emailController.text,
-            };
-            firestore.collection('users').doc().set(data);
-            // firestore.collection('users').doc("1").set(data);
-          }, child: Text("Submit"))
-        ],
+
+            TextFormField(controller: emailController),
+            ElevatedButton(
+                onPressed: () async {
+                  // FirebaseDatabase.instance.ref().child("").set(value);
+                  var data = {
+                    "firstname": fnameController.text,
+                    "lastname": lnameController.text,
+                    "email": emailController.text,
+                  };
+                  firestore.collection('users').doc().set(data);
+                  // firestore.collection('users').doc("1").set(data);
+                },
+                child: Text("Submit"))
+          ],
+        ),
       ),
     );
   }
